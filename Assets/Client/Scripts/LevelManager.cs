80803a524b0adc6880a3d3f1e03ff8fbcef56857
SolidGrey <greyfox212@mail.ru>
2019-06-22 00:01:06 +0300

    Version 0.5.2

--- Assets/Client/Scripts/LevelManager.cs
+++ Assets/Client/Scripts/LevelManager.cs
@@ -19,15 +19,10 @@ public class LevelManager : MonoBehaviour
         public bool isDespawn;
     }
 
-    public struct NNode
+    public struct Node
     {
         public Cell cell;
         public List<Cell> neighbors;
-    }
-
-    public struct DNode
-    {
-        public Cell cell;
         public int distance;
     }
 
@@ -35,8 +30,17 @@ public class LevelManager : MonoBehaviour
     {
         string[] gridPattern = LoadGridPattern(1);
         Cell[,] cellMatrix = BuildLevel(gridPattern);
-        List<NNode> graph = BuildGraph(cellMatrix);
-        FindRoutes(graph);
+        List<Node> graph = BuildGraph(cellMatrix);
+        List<List<Transform>> routes = FindRoutes(graph);
+
+        for (int i = 0; i <routes.Count; i++)
+        {
+            for (int j = 0; j < routes[i].Count; j++)
+            {
+                Debug.Log("x:" + routes[i][j].position.x + " z:" + routes[i][j].position.z);
+            }
+            Debug.Log("----------------------");
+        }
     }
 
     // Import level from resources. Input parameter requires level number
@@ -75,18 +79,18 @@ public class LevelManager : MonoBehaviour
                         Instantiate(field, position, Quaternion.identity, map.transform);
                         break;
                     case '1':
-                        matrix[i,j].transform = Instantiate(road, position, Quaternion.identity, map.transform).transform;
-                        matrix[i,j].isRoad = true;
+                        matrix[i, j].transform = Instantiate(road, position, Quaternion.identity, map.transform).transform;
+                        matrix[i, j].isRoad = true;
                         break;
                     case 'I':
-                        matrix[i,j].transform = Instantiate(spawner, position, Quaternion.identity, map.transform).transform;
-                        matrix[i,j].isSpawn = true;
-                        matrix[i,j].isRoad = true;
+                        matrix[i, j].transform = Instantiate(spawner, position, Quaternion.identity, map.transform).transform;
+                        matrix[i, j].isSpawn = true;
+                        matrix[i, j].isRoad = true;
                         break;
                     case 'O':
-                        matrix[i,j].transform = Instantiate(despawner, position, Quaternion.identity, map.transform).transform;
-                        matrix[i,j].isDespawn = true;
-                        matrix[i,j].isRoad = true;
+                        matrix[i, j].transform = Instantiate(despawner, position, Quaternion.identity, map.transform).transform;
+                        matrix[i, j].isDespawn = true;
+                        matrix[i, j].isRoad = true;
                         break;
                 }
                 xPosition += 5f;
@@ -94,30 +98,30 @@ public class LevelManager : MonoBehaviour
             zPosition -= 5f;
             xPosition = xStartPosition;
         }
-        return matrix;   
+        return matrix;
     }
 
     //Use cell matrix to build road graph
-    List<NNode> BuildGraph (Cell[,] matrix)
+    List<Node> BuildGraph(Cell[,] matrix)
     {
-        List<NNode> nodes = new List<NNode>();
+        List<Node> nodes = new List<Node>();
         for (int i = 0; i < matrix.GetLength(0); i++)
         {
             for (int j = 0; j < matrix.GetLength(1); j++)
             {
-                if(matrix[i,j].isRoad)
+                if (matrix[i, j].isRoad)
                 {
                     //Look for cells adjacent to the selected
                     List<Cell> cellNeighbors = new List<Cell>();
-                    if (i > 0 && matrix[i-1,j].isRoad)
-                        cellNeighbors.Add(matrix[i-1,j]);
-                    if (j < matrix.GetLength(1)-1 && matrix[i,j+1].isRoad)
-                        cellNeighbors.Add(matrix[i,j+1]);
-                    if (i < matrix.GetLength(0)-1 && matrix[i+1,j].isRoad)
-                        cellNeighbors.Add(matrix[i+1,j]);
-                    if (j > 0 && matrix[i,j-1].isRoad)
-                        cellNeighbors.Add(matrix[i,j-1]);
-                    nodes.Add(new NNode() { cell = matrix[i,j], neighbors = cellNeighbors });
+                    if (i > 0 && matrix[i - 1, j].isRoad)
+                        cellNeighbors.Add(matrix[i - 1, j]);
+                    if (j < matrix.GetLength(1) - 1 && matrix[i, j + 1].isRoad)
+                        cellNeighbors.Add(matrix[i, j + 1]);
+                    if (i < matrix.GetLength(0) - 1 && matrix[i + 1, j].isRoad)
+                        cellNeighbors.Add(matrix[i + 1, j]);
+                    if (j > 0 && matrix[i, j - 1].isRoad)
+                        cellNeighbors.Add(matrix[i, j - 1]);
+                    nodes.Add(new Node() { cell = matrix[i, j], neighbors = cellNeighbors });
                 }
             }
         }
@@ -125,39 +129,42 @@ public class LevelManager : MonoBehaviour
     }
 
     //Return List with routes for enemy
-    List<List<Cell>> FindRoutes(List<NNode> nGraph) //not ready
+    List<List<Transform>> FindRoutes(List<Node> graph) //not ready
     {
-        List<List<Cell>> routes = new List<List<Cell>>();
+        List<List<Transform>> routes = new List<List<Transform>>();
 
-        //Search despawners and build distances graphs
-        List<List<DNode>> dGraphs = new List<List<DNode>>();
-        for (int i = 0; i < nGraph.Count; i++)
-            if (nGraph[i].cell.isDespawn)
-                dGraphs.Add(buildDistancesGraph(i));
-        return null;
+        //Search despawners
+        for (int i = 0; i < graph.Count; i++)
+            if (graph[i].cell.isDespawn)
+            {
+                List<Node> distancesGraph = buildDistancesGraph(i); // Return graph with nodes that have a distances
+                List<Transform> startRoute = new List<Transform>();
+                fillingRoutes(distancesGraph, startRoute, distancesGraph[0]);
+            }
+        return routes;
 
-        //Build distances graph. Use nodes graph
-        List<DNode> buildDistancesGraph(int despawnIndex)
+        //Build distances graph
+        List<Node> buildDistancesGraph(int despawnIndex)
         {
-            List<DNode> dGraph = new List<DNode>();
-            int[] distances = new int[nGraph.Count];
+            List<Node> dGraph = new List<Node>(); // new distances graph 
+            int[] distances = new int[graph.Count];
             Queue<int> queue = new Queue<int>();
             distances[despawnIndex] = 1; //1 despawner start distance
             queue.Enqueue(despawnIndex);
-            dGraph.Add(new DNode { cell = nGraph[despawnIndex].cell, distance = distances[despawnIndex] });
+            dGraph.Add(new Node { cell = graph[despawnIndex].cell, neighbors = graph[despawnIndex].neighbors, distance = distances[despawnIndex] });
             while (queue.Count > 0)
             {
                 int firstIndex = queue.Dequeue();
                 //Search neighbors of cell with first index in queue
-                for (int i = 0; i < nGraph[firstIndex].neighbors.Count; i++)
+                for (int i = 0; i < graph[firstIndex].neighbors.Count; i++)
                 {
-                    for (int j = 0; j < nGraph.Count; j++)
+                    for (int j = 0; j < graph.Count; j++)
                     {
-                        if (nGraph[firstIndex].neighbors[i].transform == nGraph[j].cell.transform && distances[j] == 0)
+                        if (graph[firstIndex].neighbors[i].transform == graph[j].cell.transform && distances[j] == 0)
                         {
                             queue.Enqueue(j);
                             distances[j] = distances[firstIndex] + 1;
-                            dGraph.Add(new DNode { cell = nGraph[j].cell, distance = distances[j] });
+                            dGraph.Add(new Node { cell = graph[j].cell, neighbors = graph[j].neighbors, distance = distances[j] });
                         }
                     }
                 }
@@ -165,6 +172,42 @@ public class LevelManager : MonoBehaviour
             return dGraph;
         }
 
+        void fillingRoutes(List<Node> dGraph, List<Transform> prevRoute, Node startNode)
+        {
+            Node currentNode = startNode;
+            List<Transform> route = new List<Transform>();
+            route.AddRange(prevRoute);
+            do
+            {
+                bool oneBrunch = true;
+                Node nextNode = currentNode;
+                for (int i = 0; i < currentNode.neighbors.Count; i++) //Finding neighbors
+                {
+                    for (int j = 0; j < dGraph.Count; j++) //Finding neighbor node
+                    {
+                        if (currentNode.neighbors[i].transform == dGraph[j].cell.transform && currentNode.distance < dGraph[j].distance) //Add Transform to route if his distance more then current node
+                        {
+                            if (oneBrunch)
+                            {
+                                route.Add(currentNode.cell.transform);
+                                nextNode = dGraph[j];
+                                oneBrunch = false;
+                            }
+                            else
+                                fillingRoutes(dGraph, route, dGraph[j]);
+                        }
+                    }
+                }
+                currentNode = nextNode;
+            } while (currentNode.neighbors.Count > 1);
+
+            if (currentNode.cell.isSpawn)
+            {
+                route.Add(currentNode.cell.transform);
+                routes.Add(route);
+            }
+                
+        }
     }
-    
+
 }
